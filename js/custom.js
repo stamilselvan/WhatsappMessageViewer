8bfc81a0506e7f4cbad4d96be1a8712df7845efd
stamilselvan <yestamilselvan@gmail.com>
2019-06-16 21:45:39 +0200

    Add date

--- js/custom.js
+++ js/custom.js
@@ -5,6 +5,9 @@ if (window.File && window.FileReader && window.FileList) {
   alert('The File APIs are not fully supported in this browser.');
 }
 
+var dateOptions = { year: 'numeric', month: 'long', day: 'numeric' };
+
+var text;
 var openFile = function(event) {
 	var input = event.target;
 
@@ -12,7 +15,36 @@ var openFile = function(event) {
 	reader.readAsText(input.files[0]);
 
 	reader.onload = function() {
-		var text = reader.result;
+		text = reader.result;
 		document.getElementById("result").innerText = text;
+		parseInput(text);
 	};
 };
+
+function parseInput(input) {
+	var lineArray = input.split("\n");
+	for (var lineIndex = 0; lineIndex < lineArray.length; lineIndex++) {
+		var line = lineArray[lineIndex];
+		var commaIndex = line.indexOf(',');
+		if (commaIndex == -1) {
+			//Add to previous message
+			continue;
+		}
+		var dateAsStr = line.substr(0, commaIndex);
+		var msgDate = new Date(dateAsStr);
+		if (msgDate == "Invalid Date") {
+			//Add to previous message
+			continue;
+		}
+		var dateInFormat = msgDate.toLocaleDateString("en-US", dateOptions);
+		addDateToDom(dateInFormat);
+	}
+}
+
+function addDateToDom(date) {
+	var container = document.getElementById("view-container");
+	var newElem = document.createElement("div");
+	newElem.classList = "date"
+	newElem.append(date);
+	container.append(newElem);
+}
